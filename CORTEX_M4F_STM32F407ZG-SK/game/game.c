88a9d5640885bc4c9488932891251a1b6b806947
yenchen <th33ts@gmail.com>
2014-08-19 14:51:03 +0800

    把乒乓球變成發射台。你可以利用User Btn來移動，觸摸螢幕來發射球，射中對面不斷移動的板子，板子就會改變顏色。

--- CORTEX_M4F_STM32F407ZG-SK/game/game.c
+++ CORTEX_M4F_STM32F407ZG-SK/game/game.c
@@ -20,7 +20,7 @@ int16_t player2X = LCD_PIXEL_WIDTH - 20;
 int16_t player2Y = LCD_PIXEL_HEIGHT - 20;
 uint16_t player2W = 60;
 uint16_t player2H = 10;
-uint8_t player2IsReversed = 0;
+uint8_t player2IsReversed = 1;
 
 //Ball
 uint16_t ballSize = 5;
@@ -30,20 +30,41 @@ int16_t ballVX = 5;
 int16_t ballVY = 5;
 uint8_t ballIsRun = 0;
 
+//Balls
+typedef struct Ball {
+	int16_t X;
+	int16_t Y;
+	int16_t VX;
+	int16_t VY;
+	int8_t visible;
+	uint8_t isRun;
+} Ball;
+
+
+#define MAX_BALL_NUM 5
+Ball balls[MAX_BALL_NUM];
+uint8_t numOfBalls = 0;
+uint8_t hasInitBalls = 0;
+
+uint8_t hitTimes = 0;
+uint16_t player2Color = LCD_COLOR_WHITE;
+
+
+void
+initBalls()
+{
+	uint8_t i = 0;
+	for(; i<MAX_BALL_NUM; i++){
+		balls[i].visible = 0;
+		balls[i].isRun = 0;
+	}
+	hasInitBalls = 1;
+}
+
+
 //Mode
 uint8_t demoMode = 0;
 
-void
-BallReset()
-{
-	ballX = ( LCD_PIXEL_WIDTH - 5 ) / 2;
-	ballY = ( LCD_PIXEL_HEIGHT - 5 ) / 2;
-
-	ballVX = 5;
-	ballVY = 5;
-
-	ballIsRun = 1;
-}
 
 void
 GAME_EventHandler1()
@@ -61,23 +82,43 @@ GAME_EventHandler1()
 void
 GAME_EventHandler2()
 {
+	if ( !hasInitBalls ) {
+		initBalls();
+	}
 	if( IOE_TP_GetState()->TouchDetected ){
-
+		uint8_t i = 0;
+		for (;i<MAX_BALL_NUM; i++) {
+			if ( !balls[i].visible ) {
+				balls[i].X = player1X + player1W/2;
+				balls[i].Y = player1Y;
+				balls[i].VX = 0;
+				balls[i].VY = 5;
+				balls[i].visible = 1;
+				balls[i].isRun = 1;
+				break;
+			}
+		}
+	}
+/*
 		player2IsReversed = 1;
 
 		while( IOE_TP_GetState()->TouchDetected );
 
 		player2IsReversed = 0;
 	}
+*/
 }
+
 
 void
 GAME_EventHandler3()
 {
+	/*
 	if( ballIsRun == 0 ){
 		BallReset();
-	}
+	}*/
 }
+
 
 void
 GAME_Update()
@@ -105,205 +146,68 @@ GAME_Update()
 		else
 			player2X += 5;
 
-		if( player2X <= 0 )
+		if( player2X <= 0 ) {
 			player2X = 0;
-		else if( player2X + player2W >= LCD_PIXEL_WIDTH )
-			player2X = LCD_PIXEL_WIDTH - player2W;
-
-		//Ball
-		if( ballIsRun == 1 ){
-
-			LCD_SetTextColor( LCD_COLOR_BLACK );
-			LCD_DrawFullRect( ballX, ballY, ballSize, ballSize );
-
-			//Touch wall
-			ballX += ballVX;
-			if( ballX <= 0 ){
-				ballX = 0;
-				ballVX *= -1;
-			}
-			else if( ballX + ballSize >= LCD_PIXEL_WIDTH ){
-				ballX = LCD_PIXEL_WIDTH - ballSize;
-				ballVX *= -1;
-			}
-
-			//PONG!
-			ballY += ballVY;
-			if( ballY + ballSize >= player2Y ){
-				if( ballX + ballSize >= player2X && ballX <= player2X + player2W ){
-					if( ballX - ballSize <= player2Y + player2W/4 ){
-						ballVY =-3;
-						ballVX =-7;
-					}
-					else if( ballX >= player2Y + player2W - player2W/4 ){
-						ballVY =-3;
-						ballVX = 7;
-					}
-					else if( ballX + ballSize < player2Y + player2W/2 ){
-						ballVY =-7;
-						ballVX =-3;
-					}
-					else if( ballX > player2Y + player2W/2 ){
-						ballVY =-7;
-						ballVX = 3;
-					}
-					else{
-						ballVY =-9;
-						ballVX = 0;
-					}
-				}
-				else
-					BallReset();
-			}
-
-			if( ballY <= player1Y + player1H ){
-					if( ballX + ballSize >= player1X && ballX <= player1X + player1W ){
-						if( ballX - ballSize <= player1Y + player1W/4 ){
-							ballVY = 3;
-							ballVX =-7;
-						}
-						else if( ballX >= player1Y + player1W - player1W/4 ){
-							ballVY = 3;
-							ballVX = 7;
-						}
-						else if( ballX + ballSize < player1Y + player1W/2 ){
-							ballVY = 7;
-							ballVX =-3;
-						}
-						else if( ballX > player1Y + player1W/2 ){
-							ballVY = 7;
-							ballVX = 3;
-						}
-						else{
-							ballVY = 9;
-							ballVX = 0;
-						}
-					}
-					else
-						BallReset();
-				}
-			}
+			player2IsReversed = 0;
 		}
-		else{	//if demoMode == 1
-
-			//Player1 move
-			if( ballVY < 0 ){
-				if( player1X + player1W/2 < ballX + ballSize/2 ){
-					player1X += 8;
-					player2X += 2;
-				}
-				else{
-					player1X -= 8;
-					player2X -= 2;
-				}
-			}
-
-			//Player2 move
-			if( ballVY > 0 ){
-				if( player2X + player2W/2 < ballX + ballSize/2 ){
-					player1X += 2;
-					player2X += 8;
-				}
-				else{
-					player1X -= 2;
-					player2X -= 8;
-				}
-
-			}
-
-			if( player1X <= 0 )
-				player1X = 0;
-			else if( player1X + player1W >= LCD_PIXEL_WIDTH )
-				player1X = LCD_PIXEL_WIDTH - player1W;
-
-			if( player2X <= 0 )
-				player2X = 0;
-			else if( player2X + player2W >= LCD_PIXEL_WIDTH )
-				player2X = LCD_PIXEL_WIDTH - player2W;
+		else if( player2X + player2W >= LCD_PIXEL_WIDTH ) {
+			player2X = LCD_PIXEL_WIDTH - player2W;
+			player2IsReversed = 1;
+		}
 
 
-			//Ball
-			if( ballIsRun == 1 ){
-
+		//修改所有的球的位置
+		uint8_t i=0;
+		for (; i<MAX_BALL_NUM; i++) {
+			if (balls[i].visible && balls[i].isRun) {
 				LCD_SetTextColor( LCD_COLOR_BLACK );
-				LCD_DrawFullRect( ballX, ballY, ballSize, ballSize );
-
-				//Touch wall
-				ballX += ballVX;
-				if( ballX <= 0 ){
-					ballX = 0;
-					ballVX *= -1;
+				LCD_DrawFullRect( balls[i].X, balls[i].Y, ballSize, ballSize );
+				balls[i].X += balls[i].VX;
+				balls[i].Y += balls[i].VY;
+				if (balls[i].X >= player2X && balls[i].X <= player2X + player2W &&
+					balls[i].Y >= player2Y && balls[i].Y <= player2Y + player2H) {
+					if (hitTimes == 4) {
+						hitTimes = 0;
+					} else {
+						hitTimes++;
+					}
+					player2Color -= 0x007F;
+					if(player2Color < 0) {
+						player2Color = LCD_COLOR_WHITE;
+					}
+					balls[i].visible = 0;
+					balls[i].isRun = 0;
 				}
-				else if( ballX + ballSize >= LCD_PIXEL_WIDTH ){
-					ballX = LCD_PIXEL_WIDTH - ballSize;
-					ballVX *= -1;
+				if (balls[i].Y + ballSize >= LCD_PIXEL_HEIGHT) {
+					balls[i].visible = 0;
+					balls[i].isRun = 0;
+					break;
 				}
-
-				//PONG!
-				ballY += ballVY;
-				if( ballY + ballSize >= player2Y ){
-					if( ballX + ballSize >= player2X && ballX <= player2X + player2W ){
-					if( ballX - ballSize <= player2Y + player2W/4 ){
-						ballVY =-3;
-						ballVX =-7;
-					}
-					else if( ballX >= player2Y + player2W - player2W/4 ){
-						ballVY =-3;
-						ballVX = 7;
-					}
-					else if( ballX + ballSize < player2Y + player2W/2 ){
-						ballVY =-7;
-						ballVX =-3;
-					}
-					else if( ballX > player2Y + player2W/2 ){
-						ballVY =-7;
-						ballVX = 3;
-					}
-					else{
-						ballVY =-9;
-						ballVX = 0;
-					}
-				}
-				else
-					BallReset();
-			}
-
-			if( ballY <= player1Y + player1H ){
-				if( ballX + ballSize >= player1X && ballX <= player1X + player1W ){
-					if( ballX - ballSize <= player1Y + player1W/4 ){
-						ballVY = 3;
-						ballVX =-7;
-					}
-					else if( ballX >= player1Y + player1W - player1W/4 ){
-						ballVY = 3;
-						ballVX = 7;
-					}
-					else if( ballX + ballSize < player1Y + player1W/2 ){
-						ballVY = 7;
-						ballVX =-3;
-					}
-					else if( ballX > player1Y + player1W/2 ){
-						ballVY = 7;
-						ballVX = 3;
-					}
-					else{
-						ballVY = 9;
-						ballVX = 0;
-					}
-				}
-				else
-					BallReset();
 			}
 		}
 	}
 }
 
-	void
+void drawAllBalls()
+{
+	uint8_t i = 0;
+	for (; i<MAX_BALL_NUM; i++){
+		if (balls[i].visible) {
+			LCD_DrawFullRect(balls[i].X, balls[i].Y, ballSize, ballSize);
+		}
+	}
+}
+
+
+
+void
 GAME_Render()
 {
 	LCD_SetTextColor( LCD_COLOR_WHITE );
 	LCD_DrawFullRect( player1X, player1Y, player1W, player1H );
+	LCD_SetTextColor( player2Color );
 	LCD_DrawFullRect( player2X, player2Y, player2W, player2H );
-	LCD_DrawFullRect( ballX, ballY, ballSize, ballSize );
+	LCD_SetTextColor( LCD_COLOR_WHITE );
+	drawAllBalls();
 	LCD_DrawLine( 10, LCD_PIXEL_HEIGHT / 2, LCD_PIXEL_WIDTH - 20, LCD_DIR_HORIZONTAL );
 }
